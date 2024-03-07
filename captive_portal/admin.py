c672c55873cd1c314430895261c533a06933bd42
bolivarmenezes <menezes.bolivar@gmail.com>
2024-03-07 07:35:13 -0300

    adicionando informações no arquivo admin

--- captive_portal/admin.py
+++ captive_portal/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SessionGov, UserGov, UserPreference
+
+
+class UserGovAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cpf', 'email', 'login_wifi', 'internal_user')
+
+
+class UserPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('cpf', 'termos', 'date')
+
+
+class SessionGovAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'cpf', 'session_id', 'date', 'internal_user')
+
+
+class LogsWifiAdmin(admin.ModelAdmin):
+    list_display = ('mac', 'cpf', 'cisco_controller', 'date')
+
+
+admin.site.register(UserGov, UserGovAdmin)
+admin.site.register(UserPreference, UserPreferenceAdmin)
+admin.site.register(SessionGov, SessionGovAdmin)
